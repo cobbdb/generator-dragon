925f8a833d0ccceafe0f3fab8a869a2956b634cc
cobbdb <cobbdb@gmail.com>
2015-01-03 23:17:56 -0800

    use arr for screen templ

--- screen/templates/_screen.js
+++ screen/templates/_screen.js
@@ -3,7 +3,9 @@ var Dragon = require('dragonjs'),
 
 module.exports = Screen({
     name: '<%= name %>',
-    collisionSets: require('../collisions/ex1.js'),
+    collisionSets: [
+        require('../collisions/ex1.js')
+    ],
     spriteSet: [
         require('../sprites/ex1.js'),
         require('../sprites/ex2.js')
